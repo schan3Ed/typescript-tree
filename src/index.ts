a6c42918e8055105008c1369e7da73469229dcb9
Ed Chan <schan3edd@gmail.com>
2024-06-26 11:20:30 -0400

    find function, plus test

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export class Node {
 
   constructor(name: string) {
     this.Key = name;
+    this.Children = [];
   }
 
   addChild(child: Node) {
@@ -13,10 +14,26 @@ export class Node {
 
   removeChild(name: string) {}
 
-  find(name: string) {}
+  find(name: string): Node {
+    // DFS search
+    let tobeVisit: Node[] = [];
+    tobeVisit.push(this);
+
+    while (tobeVisit.length) {
+      const current = tobeVisit.pop();
+
+      console.log(current.Key);
+      if (current.Key == name) {
+        return current;
+      }
+
+      tobeVisit = tobeVisit.concat(current.Children.reverse());
+    }
+  }
 
   exportToFile(fileName: string) {}
 
   importFromFile(fileName: string) {}
-
 }
+
+export default Node;
